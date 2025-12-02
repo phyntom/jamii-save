718c2c05795a31cb94d97a9b3b7c7a6e930169b8
Richy-irad <36984830+Richy-irad@users.noreply.github.com>
2025-12-02 18:16:01 -0500

    resolve conflicts

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,13 +1,8 @@
 export * from './schemas/auth';
 export * from './schemas/community';
 export * from './schemas/community-loan-term';
-<<<<<<< HEAD
 export * from './schemas/invitation';
 export * from './schemas/member';
-=======
-export * from './schemas/invitations';
-export * from './schemas/members';
->>>>>>> 02c7d98 (Update dependencies, refactor community-related components, and enhance UI)
 // Export other tables
 export * from './schemas/plan';
 export * from './schemas/subscription-type';
